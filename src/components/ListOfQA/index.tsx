4186d709078561706bdf751ed1302db092341823
shikha <shikha.hotwani@knoxpo.com>
2023-02-27 13:11:13 +0530

    buttons added

--- src/components/ListOfQA/index.tsx
+++ src/components/ListOfQA/index.tsx
@@ -2,53 +2,87 @@ import { useDispatch, useSelector } from "react-redux";
 import { Questionaire, QuestionAnswerList } from "../../redux/model";
 import classes from "./style.module.css";
 import { delQA } from "../../redux/questionSlice";
+import deleteButton from "./assets/delete.png";
+import down from "./assets/downButton.png";
+import up from "./assets/upButton.png";
 
 export const ListOfQA = () => {
-    const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const state = useSelector((state: any) => state.qList);
-  const onDeleteHandler=(e:string)=>{
-    dispatch(delQA(e))
-  }
+  const onDeleteHandler = (e: string) => {
+    dispatch(delQA(e));
+  };
+
+  const onUpHandler = (e: string) => {};
+  const onDownHandler = (e: string) => {};
   return (
     <>
-    {state.list.length >0 && <h5>List Of all the Questions</h5>}
-      {state.list.map((e:{id:string,question:string,answer:string},index:string) => {
-        return (
-          <div className={classes.main} key={index}>
-            <form>
-              <label className={classes.label}>Question</label>
-              <br />
-              <input
-                id="question"
-                name="question"
-                type="text"
-                disabled
-                className={classes.text}
-                placeholder="What do you want to ask?"
-                value={e.question}
-              ></input>
-              <br />
-              <br />
+      {state.list.length > 0 && <h5>List Of all the Questions</h5>}
+      {state.list.map(
+        (
+          e: { id: string; question: string; answer: string },
+          index: string
+        ) => {
+          return (
+            <div className={classes.main} key={index}>
+              <form>
+                <label className={classes.label}>Question</label>
+                <br />
+                <input
+                  id="question"
+                  name="question"
+                  type="text"
+                  disabled
+                  className={classes.text}
+                  placeholder="What do you want to ask?"
+                  value={e.question}
+                ></input>
+                <br />
+                <br />
 
-              <label className={classes.label}>Answer</label>
-              <br />
-              <select className={classes.select} disabled>
-                <option value="Short">Short Answer</option>
-              </select>
-              <input
-                type="text"
-                disabled
-                className={`${classes.text} ${classes.answer} `}
-                placeholder="Short Answer Text"
-                value={e.answer}
-              ></input>
-              <br />
-              <button className={classes.del} type='button' onClick={() => onDeleteHandler(e.id)}>Delete</button>
-              <br />
-            </form>
-          </div>
-        );
-      })}
+                <label className={classes.label}>Answer</label>
+                <br />
+                <select className={classes.select} disabled>
+                  <option value="Short">Short Answer</option>
+                </select>
+                <input
+                  type="text"
+                  disabled
+                  className={`${classes.text} ${classes.answer} `}
+                  placeholder="Short Answer Text"
+                  value={e.answer}
+                ></input>
+                <br />
+                <div className={classes.buttons}>
+                  <button
+                    className={classes.del}
+                    type="button"
+                    onClick={() => onDeleteHandler(e.id)}
+                  >
+                    <img src={deleteButton}></img>
+                  </button>
+                  <button
+                    className={classes.del}
+                    type="button"
+                    onClick={() => onDownHandler(e.id)}
+                  >
+                    <img src={down}></img>
+                  </button>
+                  <button
+                    className={classes.del}
+                    type="button"
+                    onClick={() => onUpHandler(e.id)}
+                  >
+                    <img src={up}></img>
+                  </button>
+                </div>
+
+                <br />
+              </form>
+            </div>
+          );
+        }
+      )}
     </>
   );
 };
--- src/components/QABox/index.tsx
+++ src/components/QABox/index.tsx
@@ -1,62 +1,90 @@
-import { InputHTMLAttributes, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import classes from './style.module.css';
-import { addQA } from '../../redux/questionSlice';
-import ListOfQA from '../ListOfQA';
+import { InputHTMLAttributes, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./style.module.css";
+import { addQA } from "../../redux/questionSlice";
+import ListOfQA from "../ListOfQA";
 
-export const QABox:React.FC<{}> = (props) =>{
-    const [question,setQuestion]=useState("");
-    const state = useSelector((state: any) => state.qList);
-    
-    const dispatch = useDispatch();
-    const[answer,setAnswer]=useState("") 
-    const onClickHandler = () =>{
-        dispatch(addQA({question,answer}))
-        setQuestion("");
-        setAnswer("")
-    }
-    const onChangeQuestion = (e:React.ChangeEvent<HTMLInputElement>) =>{
-        setQuestion(e.target.value);
-    }
-    const onBlurQuestion = (e:React.FocusEvent<HTMLInputElement>) => {
-        setQuestion(e.target.value);
-        //props.onBlurQuestion(question);
-        
-        
-    }
-    const onClickSaveHandler = () =>{
-        console.log(JSON.stringify(state.list))
-    }
-    const onChangeAnswer = (e:React.ChangeEvent<HTMLInputElement>) =>{
-        setAnswer(e.target.value);
-    }
-    const onBlurAnswer = (e:React.FocusEvent<HTMLInputElement>) => {
-        setAnswer(e.target.value);
-        //props.onBlurAnswer(answer);
-        
-        
-    }
-    return(<>
-    <div className={classes.main}>
-       <form>
-        <label htmlFor="question" className={classes.label}>Question</label><br/>
-        <input name="question" id="question" type='text' className={classes.text} value={question} onChange={onChangeQuestion} onBlur={onBlurQuestion}  placeholder="What do you want to ask?"></input><br/><br/>
+export const QABox: React.FC<{}> = (props) => {
+  const [question, setQuestion] = useState("");
+  const state = useSelector((state: any) => state.qList);
 
-        <label htmlFor='answer' className={classes.label}>Answer</label><br/>
-        <select className={classes.select}>
-        <option value="Short" >Short Answer</option>
-        </select>
-        <input type='text' name='answer' id='answer' className={`${classes.text} ${classes.answer} `} value={answer} onChange={onChangeAnswer} onBlur={onBlurAnswer} placeholder="Short Answer Text"></input><br/><br/>
+  const dispatch = useDispatch();
+  const [answer, setAnswer] = useState("");
+  const onClickHandler = () => {
+    dispatch(addQA({ question, answer }));
+    setQuestion("");
+    setAnswer("");
+  };
+  const onChangeQuestion = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+  };
+  const onBlurQuestion = (e: React.FocusEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+    //props.onBlurQuestion(question);
+  };
+  const onClickSaveHandler = () => {
+    console.log(JSON.stringify(state.list));
+  };
+  const onChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+  };
+  const onBlurAnswer = (e: React.FocusEvent<HTMLInputElement>) => {
+    setAnswer(e.target.value);
+    //props.onBlurAnswer(answer);
+  };
+  return (
+    <>
+      <div className={classes.main}>
+        <form>
+          <label htmlFor="question" className={classes.label}>
+            Question
+          </label>
+          <br />
+          <input
+            name="question"
+            id="question"
+            type="text"
+            className={classes.text}
+            value={question}
+            onChange={onChangeQuestion}
+            onBlur={onBlurQuestion}
+            placeholder="What do you want to ask?"
+          ></input>
+          <br />
+          <br />
 
-    </form>
-    
-    </div>
-    <button className={classes.add} onClick={onClickHandler} >+ ADD QUESTION</button><br/>
-    <button className={classes.save} onClick={onClickSaveHandler} >SAVE AND SHARE</button><br/>
-    <ListOfQA/>
-        </>
-        
-    )
-}
+          <label htmlFor="answer" className={classes.label}>
+            Answer
+          </label>
+          <br />
+          <select className={classes.select}>
+            <option value="Short">Short Answer</option>
+          </select>
+          <input
+            type="text"
+            name="answer"
+            id="answer"
+            className={`${classes.text} ${classes.answer} `}
+            value={answer}
+            onChange={onChangeAnswer}
+            onBlur={onBlurAnswer}
+            placeholder="Short Answer Text"
+          ></input>
+          <br />
+          <br />
+        </form>
+      </div>
+      <button className={classes.add} onClick={onClickHandler}>
+        + ADD QUESTION
+      </button>
+      <br />
+      <button className={classes.save} onClick={onClickSaveHandler}>
+        SAVE AND SHARE
+      </button>
+      <br />
+      <ListOfQA />
+    </>
+  );
+};
 
 export default QABox;
